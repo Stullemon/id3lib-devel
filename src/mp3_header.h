495fa6522b39e36d06127b894ae6a411eca0b9ca
t1mpy <t1mpy>
2002-09-13 15:40:04 +0000

    Added datasize in mp3 header struct

--- src/mp3_header.h
+++ src/mp3_header.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: mp3_header.h,v 1.3 2002/07/31 12:47:42 t1mpy Exp $
+// $Id: mp3_header.h,v 1.4 2002/09/13 15:40:04 t1mpy Exp $
 
 // id3lib: a C++ library for creating and manipulating id3v1/v2 tags
 // Copyright 2002 Thijmen Klok (thijmen@id3lib.org)
@@ -33,10 +33,10 @@
 class Mp3Info
 {
 public:
-  Mp3Info() { _mp3_header_output = new Mp3_Headerinfo; };
+  Mp3Info() { _mp3_header_output = LEAKTESTNEW(Mp3_Headerinfo); };
   ~Mp3Info() { this->Clean(); };
   void Clean();
-    
+
   const Mp3_Headerinfo* GetMp3HeaderInfo() const { return _mp3_header_output; };
   bool Parse(ID3_Reader&, size_t mp3size);
 
@@ -54,6 +54,7 @@ public:
   bool Copyrighted() const { return _mp3_header_output->copyrighted; };
   bool Original() const { return _mp3_header_output->original; };
   uint32 Seconds() const { return _mp3_header_output->time; };
+  uint32 DataSize() const { return _mp3_header_output->datasize; };
 
 private:
 
